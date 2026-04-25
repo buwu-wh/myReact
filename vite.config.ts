d63c987fc93021e6908f578cd5edda7047b14f6f
buwu-wh <444948924@qq.com>
2026-04-25 16:34:36 +0800

    CI/CD配置测试.sh 的export语法

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+console.log('新发现啊process.env.REACT_APP_ENV', process.env.REACT_APP_ENV);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
