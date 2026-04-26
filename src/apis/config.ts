34d66595540efb87f6ecd37aea639ec4c70058ad
buwu-wh <444948924@qq.com>
2026-04-26 09:24:01 +0800

    路由配置修改

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,5 +1,5 @@
 function getBaseUrl() {
   console.log(location.protocol + 'location.protocol');
-  return location.protocol + '//localhost:5173/';
+  return location.protocol + '//localhost:5173/myReact/';
 }
 export default { getBaseUrl };
